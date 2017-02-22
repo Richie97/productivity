7f58f3d727e720c5feb52e1655a2407f34af15ac
Eric Richardson <eric.richardson97@gmail.com>
2017-02-22 11:59:33 -0800

    Added some stub activities and such

---
+++ app/src/main/java/com/dimensions/productivity/activity/OverviewActivity.java
@@ -0,0 +1,15 @@
+package com.dimensions.productivity.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dimensions.productivity.R;
+
+public class OverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+    }
+}
