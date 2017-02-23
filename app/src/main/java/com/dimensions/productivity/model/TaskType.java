b5d79f482126d1184dd81b30f2365d674bd7ed64
Nick Butcher <nickbutcher@google.com>
2017-02-23 10:23:29 -0800

    Added more fake data.

--- app/src/main/java/com/dimensions/productivity/model/TaskType.java
+++ app/src/main/java/com/dimensions/productivity/model/TaskType.java
@@ -7,10 +7,10 @@ import com.dimensions.productivity.R;
 
 public enum TaskType {
     TRELLO(R.drawable.ic_trello, 0xff026aa7),
-    KEEP(R.drawable.ic_google_keep, 0xffffbb00),
+    KEEP(R.drawable.ic_google_keep, 0xffa97c01),
     BASECAMP(R.drawable.ic_basecamp, 0xff5ecc62),
     JIRA(R.drawable.ic_jira, 0xff0d3f72),
-    CALENDAR(R.drawable.ic_calendar, 0xff3367d6);
+    CALENDAR(R.drawable.ic_calendar, 0xff3a7ef5);
 
     @DrawableRes public final int icon;
     @ColorInt public final int color;
