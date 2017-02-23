67bbc2b3cc6393247d80fef2ef944ce761777a1f
Jeremy Willemse <jeremy@momentussoftware.com>
2017-02-23 13:25:53 -0800

    Set Overview to launch activity

--- app/src/main/java/com/dimensions/productivity/activity/MainActivity.java
+++ app/src/main/java/com/dimensions/productivity/activity/MainActivity.java
@@ -1,20 +1,17 @@
 package com.dimensions.productivity.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.dimensions.productivity.R;
-import com.dimensions.productivity.interactor.OrganizeInteractor;
-import com.dimensions.productivity.view.impl.OnboardingActivity;
-import com.dimensions.productivity.view.impl.OrganizeActivity;
+import com.dimensions.productivity.view.impl.OverviewActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, OrganizeActivity.class));
+        startActivity(new Intent(this, OverviewActivity.class));
         finish();
     }
 }
